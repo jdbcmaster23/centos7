881231241febd3568d9b46ff414770f16c67ff54
jdbcmaster23 <45805507+jdbcmaster23@users.noreply.github.com>
2019-06-20 08:54:22 +0800

    Update menuscript.sh

--- menuscript.sh
+++ menuscript.sh
@@ -21,7 +21,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/NikkoRod/sample/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/jdbcmaster23/centos7/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
